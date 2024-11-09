a9f8779c4dca0228ede59bcd3671052f2ef09db4
Saugat <saugatg189@gmail.com>
2024-11-09 19:18:48 +0545

    added

--- dbfrontend/src/components/ui/button.jsx
+++ dbfrontend/src/components/ui/button.jsx
@@ -35,7 +35,7 @@ const buttonVariants = cva(
 )
 
 const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button"
+  const Comp = asChild ? Slot : "Button"
   return (
     (<Comp
       className={cn(buttonVariants({ variant, size, className }))}
