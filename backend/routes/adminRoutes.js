122972ad3b6c4c4ec0e8cf3b1503f8f2c5c199a5
Saugat <saugatg189@gmail.com>
2024-11-09 19:57:19 +0545

    adminroutes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -4,6 +4,6 @@ import { getAllData } from "../controllers/admin.controllers.js";
 import { getUser } from "../controllers/admin.controllers.js";
 import seedMiddleware from "../middleware/seedMiddleware.js";
 
-router.get("/getalldata",seedMiddleware,getAllData)
+router.get("/getalldata",getAllData)
 router.get("/getUser",seedMiddleware,getUser)
 export default router
